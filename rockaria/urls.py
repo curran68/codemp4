e020ec2f4bfba78afdf86998ca152100fd66f8c1
curran68 <mac1968@gmail.com>
2025-09-06 20:21:51 +0100

    initial commit

---
+++ rockaria/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from rockaria import views  # Import views from your app
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),          # Root URL
+    path('alt/', views.home, name='home_alt'),  # Optional alternate URL
+]
