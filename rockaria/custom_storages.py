199424181028affb2596e951e6d295999bb529ab
curran68 <mac1968@gmail.com>
2025-09-16 21:34:00 +0100

    s3 storage config

--- rockaria/custom_storages.py
+++ rockaria/custom_storages.py
@@ -1,10 +1,14 @@
+# custom_storages.py
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = getattr(settings, 'STATICFILES_LOCATION', 'static')
+    default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = getattr(settings, 'MEDIAFILES_LOCATION', 'media')
+    file_overwrite = False
+    default_acl = 'public-read'
